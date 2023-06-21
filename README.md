cdbbd2d51a98379c723c45af4d39e14e74a658ef
yue_pu <yue_pu@en-japan.com>
2023-06-21 21:16:13 +0900

    1

---
+++ README.md
@@ -0,0 +1,6 @@
+# 取得最新的比特币价格数据 按照美元
+目前稚嫩取得最新的一个数据 不是历史数据
+
+pip install -r requirements.txt
+python main.py
+
---
+++ main.py
@@ -0,0 +1,65 @@
+import json
+import requests
+import quart
+import quart_cors
+from quart import jsonify, request
+from httpx import AsyncClient
+
+app = quart.Quart(__name__)
+quart_cors.cors(app, allow_origin="https://chat.openai.com") # 只允许chatgpt官方domin的访问
+    
+@app.route("/crypto", methods=['GET'])
+async def get_crypto():
+    url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+      'start':'1',
+      'limit':'5000',
+      'convert':'USD'
+    }
+    headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': '7f349407-9973-4ed9-946a-64d04e1467fb',
+    }
+    try:
+        async with AsyncClient() as client:
+            response = await client.get(url, params=parameters, headers=headers)
+            data = response.json()
+            return quart.Response(response=json.dumps(data), status=200)
+    except Exception as e:
+        return quart.Response(response=json.dumps({"error": str(e)}), status=400)
+    
+@app.get("/logo.png")#响应读取logo的请求
+async def plugin_logo():
+    filename = 'logo.png'
+    try:
+        return await quart.send_file(filename, mimetype='image/png')
+    except FileNotFoundError:
+        return jsonify({"error": f"文件'{filename}'不存在"}), 404
+
+@app.get("/.well-known/ai-plugin.json")#响应读取manifest文件的请求
+async def plugin_manifest():
+    host = request.headers['Host']
+    filename = "./.well-known/ai-plugin.json"
+    try:
+        with open(filename) as f:
+            text = f.read()
+            return quart.Response(text, mimetype="text/json")
+    except FileNotFoundError:
+        return jsonify({"error": f"文件'{filename}'不存在"}), 404
+
+@app.get("/openapi.yaml")#响应读取openAPI规范文件(仕様書)的请求
+async def openapi_spec():
+    host = request.headers['Host']
+    filename = "openapi.yaml"
+    try:
+        with open(filename) as f:
+            text = f.read()
+            return quart.Response(text, mimetype="text/yaml")
+    except FileNotFoundError:
+        return jsonify({"error": f"文件'{filename}'不存在"}), 404
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)#启动服务
+
+if __name__ == "__main__":
+    main()
